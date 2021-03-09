cc437d0f8f71cdfa02ed99f7425931f8067586b4
Pavel <paveltrpn@mail.ru>
2021-03-09 20:28:31 +0400

    add run terminal scripts

---
+++ run-3-terminal.sh
@@ -0,0 +1,5 @@
+#/usr/bin/bash
+
+gnome-terminal --hide-menubar --geometry=118x30+971+0 -- mc
+gnome-terminal --hide-menubar --geometry=118x30+971+571
+gnome-terminal --hide-menubar --geometry=118x63+0+0
---
+++ run-4-terminal.sh
@@ -0,0 +1,6 @@
+#/usr/bin/bash
+
+gnome-terminal --hide-menubar --geometry=118x30+0+0 -- mc
+gnome-terminal --hide-menubar --geometry=118x30+971+0 
+gnome-terminal --hide-menubar --geometry=118x30+0+571
+gnome-terminal --hide-menubar --geometry=118x30+971+571
