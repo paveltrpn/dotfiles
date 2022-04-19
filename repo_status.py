3098a6a8c92119dfa1be9e7a9a1ae9c3c7f900b0
paveltrpn <paveltrpn@mail.ru>
2022-04-19 22:19:22 +0400

    fix repo_status.py

--- repo_status.py
+++ repo_status.py
@@ -5,40 +5,9 @@
 import os
 import subprocess
 
-def checkNew(input: list[str]) -> str:
-    rt: list[str]
-    rt = []
-
-    for i, line in enumerate(input):
-        if line.find("Неотслеживаемые файлы:") != -1:
-            rt.append("Untracked files:")
-            for val in input[i+2:-4]:
-                rt.append(val)
-
-    if len(rt) == 0: 
-        return "Repo has no untracked files."
-    else:
-        return "\n".join(rt)
-
-def checkErased(input: list[str]) -> str:
-    return "Mock for check repo for erased files"
-
-def checkChanges(input: list[str]) -> str:
-    rt: list[str] = []
-
-    for line in input:
-        if line.find("изменено:") != -1:
-            rt.append(line.strip("\t"))
-
-    if len(rt) == 0: 
-        return "Repo has no changes in files."
-    else:
-        return "\n".join(rt)
-
-
 gitPorcelainCodes: dict[str, str] = {" M": "updated in index",
                                      " T": "type changed in index",
-                                     " A": "added to index",
+                                     "A ": "added to index",
                                      " D": "deleted from index",
                                      " R": "renamed in index",
                                      " C": "copied in index",
@@ -78,13 +47,6 @@ def main() -> None:
             outStrList = out.stdout.decode("utf-8").split("\n")
 
             print("{}\n".format(checkStatusWithPorcelain(outStrList)))
-
-            # changedFilesStr = checkChanges(outStrList)
-            # print(changedFilesStr)
-            # newFilesStr = checkNew(outStrList)
-            # print(newFilesStr)
-            # erasedFilesStr = checkErased(outStrList)
-            # print(erasedFilesStr)
 
             os.chdir(wd) 
 
